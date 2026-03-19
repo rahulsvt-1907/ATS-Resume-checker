0db1657b03d7211a5ffdee7b4e2f51dcca7389d4
Rahul Srivastava <rahulgsrivastava1906@gmail.com>
2026-03-19 21:32:11 +0530

    Initial clean project setup with gitignore and cleanup

---
+++ men.py
@@ -0,0 +1,12 @@
+#hello shiv shiv
+print("hello shiv shiv")
+print("hello shiv shiv")
+print("hello shiv shiv")
+print("hello shiv shiv")
+print("hello shiv shiv")
+print("hello shiv shiv")
+print("hello shiv shiv")
+
+print("hello jai shiv")
+
+print("git log --oneline")
